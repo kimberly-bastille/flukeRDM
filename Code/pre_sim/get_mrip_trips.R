502eaeb4bfb5fca611c6a1e0f9522b7a4ddfb47b
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2026-08-25 15:32:37 -0400

    sfsbsb changes.

--- Code/pre_sim/get_mrip_trips.R
+++ Code/pre_sim/get_mrip_trips.R
@@ -4,12 +4,12 @@
 #               using MRIP tacklebox. Verify that MRIP tacklebox matches known good code
 #               before switching to other harder metrics (catch by weight)
 # Inputs:       mrip_pull{}.Rds
-# Outputs:      groundfish_effort{}.Rds is a list containing estimates at different definitions
-#               of effort. For the groundfishRDM, we use the item
+# Outputs:      sfs_effort{}.Rds is a list containing estimates at different definitions
+#               of effort. For the flukeRDM, we use the item
 #                  groundfish_effort$"PRIM1|PRIM2|A|B1|B2"
 #               YEAR, WAVE, MODE, and AREA levels.
 # Dependencies: mriptacklebox
-#               Sources developer_setup.R (for gf.data.dir).
+#               Sources developer_setup.R (for sf.data.dir).
 # Pipeline:     Called by model_wrapper.do
 #                 Effort can be sent to Dashboard
 # To Do:        PSEs for Effort
@@ -36,7 +36,7 @@ conflicts_prefer(dplyr::lag)
 here::i_am("Code/pre_sim/get_mrip_trips.R")
 source(here("Code", "helpers", "developer_setup.R"))
 
-output_folder<-file.path(gf.data.dir, "miscellaneous")
+output_folder<-file.path(sf.data.dir, "miscellaneous")
 
 vintage_string<-list.files(output_folder, pattern=glob2rx("mrip_pull*Rds"))
 vintage_string<-gsub("mrip_pull","",vintage_string)
@@ -63,48 +63,16 @@ list_names <- types %>%
   map_chr(~ paste(.x, collapse = "|"))
 
 
-# Sample code to get just Cod or Just Haddock
-# targets_COD <- types %>%
-#   map(~ mrip_effort(
-#     dom = c("YEAR", "WAVE"),
-#     microdata = mrip_pull,
-#     dir_trip = list(
-#       comname = c('ATLANTIC COD'),
-#       typ = .x
-#     )
-#   )) %>%
-#   set_names(list_names)
-#
-# targets_HADDOCK <- types %>%
-#   map(~ mrip_effort(
-#     dom = c("YEAR", "WAVE"),
-#     microdata = mrip_pull,
-#     dir_trip = list(
-#       comname = c('HADDOCK'),
-#       typ = .x
-#     )
-#   )) %>%
-#   set_names(list_names)
-
-# Target or caught Either
-
-groundfish_effort <- types %>%
+sfs_effort <- types %>%
   map(~ mrip_effort(
-    dom = c("YEAR", "WAVE","MODE1", "AREA_S"),
+    dom = c("YEAR", "WAVE","MODE1", "STOCK_REGION_CALC"),
     microdata = mrip_pull,
     dir_trip = list(
-      comname =  c('ATLANTIC COD', 'HADDOCK'),
+      comname =  c('SUMMER FLOUNDER', 'SCUP', 'BLACK SEA BASS'),
       typ = .x
     )
   )) %>%
   set_names(list_names)
 
 
-write_rds(groundfish_effort,file=file.path(output_folder, glue("groundfish_effort{data_vintage}.Rds")))
-
-
-# The relationship of some of the entries
-# PRIM1 : Cod + haddock=Either by definition
-# PRIM2: Same
-# Everything else: Either>= Cod, Either>=Haddock, Either<=Cod+Haddock
-
+write_rds(sfs_effort,file=file.path(output_folder, glue("sfs_effort{data_vintage}.Rds")))
